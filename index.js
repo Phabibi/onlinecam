7cfdbb58be37e151cff7fccbc18eb62686ff8d34
parsa <phabibi@sfu.ca>
2019-08-28 16:40:15 -0700

    working

---
+++ index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const cv = require('opencv4nodejs');
+const wcam = new cv.VideoCapture(0);
+
+const farme_rate = 20;
+
+app.get('/' , (req,res) => {
+    res.sendFile(path.join(__dirname,'index.html'));
+    console.log(req)
+});
+setInterval(()=>{
+    const frame = wcam.read();
+    const image = cv.imencode('.jpg',frame).toString('base64');
+
+    io.emit('image', image)
+}, 1000 / farme_rate);
+
+console.log('listening on port 8080')
+server.listen(8080);
+
+
+
